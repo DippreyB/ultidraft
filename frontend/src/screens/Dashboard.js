1d27912b9d359d29e84ebb85289771073683365b
DippreyB <dippreyb@lisd.net>
2021-10-04 16:09:29 -0500

    Dashboad

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Dashboard = () => {
+    //show offscreen with leagues - button to show offscreen and reselect league.
+    //When league is selected, display teams & players for league.
+    //Modals for team/player info when selected? Might be best on mobile.
+    //section of screen to display player/team info on larger screens
+    //filters for players M/F, C/H, Height, Age, W/E
     return (
         <div>
-            dash
+            
         </div>
     )
 }
