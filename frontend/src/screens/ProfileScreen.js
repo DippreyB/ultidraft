116e3962795eb1017344f83448b506ee75a06d7d
DippreyB <48030605+DippreyB@users.noreply.github.com>
2021-11-30 21:57:15 -0600

    Making Profile page look better

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectLoggedInUser } from '../slices/loggedInUserSlice'
-import {Col, Container, Row} from 'react-bootstrap'
+import {Col, Container, Image, Row} from 'react-bootstrap'
 import axios from '../lib/axios'
 import OverviewCard from '../components/Profile/OverviewCard'
 import RatingCard from '../components/Profile/RatingCard'
 import LeaguesCard from '../components/Profile/LeaguesCard'
+import Identicon from 'identicon.js'
 
 const ProfileScreen = ({match}) => {
     const {loggedInUser} = useSelector(selectLoggedInUser)
@@ -31,13 +32,45 @@ const ProfileScreen = ({match}) => {
             setPlayerProfile(undefined)
         }
 
-    },[loggedInUser])
+    },[loggedInUser, match])
+
+    let data
+    if(playerProfile)
+        data = new Identicon(playerProfile._id, 300).toString()
+    
+    
 
     return (
             <Container>
-                <Row><span className='display-3'>Player Profile</span></Row>
-                
                 {playerProfile &&
+                <>
+                <Row>
+                    {/* player info column */}
+                    <Col xs={12} md={4}> 
+                        <Row>
+                            <Col className='d-flex justify-content-center' xs={4} md={12}>
+                                <Image src={`data:image/png;base64,${data}`} fluid roundedCircle></Image>
+                            </Col>
+                            <Col className='d-flex  align-items-center' xs={8} md={12}>
+                                    <div className='display-6'>{playerProfile.playerName}</div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <div>{playerProfile.age}, {playerProfile.genderMatchup}</div>
+                        </Row>
+                        <Row>
+                            <div className='text-muted'>Experience </div>
+                            <div>{playerProfile.experience} {playerProfile.role}</div>
+                        </Row>
+                        <Row>
+                            <div className='text-muted'>Comments </div>
+                            <div>{playerProfile.comments} </div>
+                        </Row>
+                    </Col>
+                </Row>
+
+
+
                 <Row className='my-2'>
                     <Col md={4}>
                         <OverviewCard playerProfile={playerProfile}/>
@@ -50,6 +83,7 @@ const ProfileScreen = ({match}) => {
                     </Col>
                    
                 </Row>
+                </>
                 }
                
             </Container>
